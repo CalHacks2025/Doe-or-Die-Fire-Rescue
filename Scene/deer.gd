685e01022a20a8cc1eae36b84717653c0d2afb60
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2025-02-15 16:09:17 -0700

    add wip hp damage, iframe, and knockback for deer, fireman

--- Scene/deer.gd
+++ Scene/deer.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
-@export var firefighter : CharacterBody2D
+@export var fireman : CharacterBody2D
 
 const SPEED = 25
 
 func _physics_process(delta: float) -> void:
-	if firefighter:
+	velocity = Vector2.ZERO
+	if fireman:
 		# Calculate direction to the player and move
-		velocity = position.direction_to(firefighter.position) * SPEED
+		velocity = position.direction_to(fireman.position) * SPEED
+		move_and_slide()
+		
+func _on_DetectRadius_body_entered(body):
+	# Damage if a fireman
+	if body == fireman:
+		fireman.hp -= 1
+
+# Distance the animal away from fireman to avoid recollision
+func _on_DetectRadius_body_exited(body):
+	if body == fireman:
+		velocity = -position.direction_to(fireman.position) * SPEED
 		move_and_slide()
