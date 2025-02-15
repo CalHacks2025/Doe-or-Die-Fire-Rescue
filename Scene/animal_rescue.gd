300586829d53c2477afcb070d045b424e8312762
imbrettirl <brett.gow@ucalgary.ca>
2025-02-15 15:10:55 -0700

    test

--- Scene/animal_rescue.gd
+++ Scene/animal_rescue.gd
@@ -4,24 +4,22 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-func move() -> void:
-	var direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("move_left_la"):
-		direction.x = -1
-	elif  Input.is_action_pressed("move_right_ra"):
-		direction.x = 1
-	
-	if Input.is_action_pressed("move_down_da"):
-		direction.y = 1
-	elif  Input.is_action_pressed("move_up_ua"):
-		direction.y = -1
-	
-	if direction.x != 0 and direction.y != 0:
-		direction = direction.normalized()
-	
-	velocity = direction * SPEED
 
 func _physics_process(delta: float) -> void:
-	move()
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
