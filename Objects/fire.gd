dfe18ff9c9f7291a58bf2d729f7076e7e65fe6d8
NguyenNhuDi <dinguyennhu@gmail.com>
2025-02-15 17:33:03 -0700

    added water bucket and fire fighting logic

--- Objects/fire.gd
+++ Objects/fire.gd
@@ -4,12 +4,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready():
+	add_to_group("fire")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
--- Players/fire_man.gd
+++ Players/fire_man.gd
@@ -3,7 +3,27 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const FIRE_DIS = 70
 
+var water_level = 10
+var fire_objects = []  # List to store objects and their distances
+var water_next_to = 0
+
+func _ready():
+	var area2D = $Area2D
+	area2D.area_entered.connect(_on_body_entered)  # Correct in Godot 4
+	area2D.area_exited.connect(_on_body_exited)
+	
+func _on_body_entered(body):
+	if body.name == "water_hit_box":
+		water_next_to += 1
+	elif body.name =="Fire_Hurtbox":
+		print("YOUCH!")
+
+func _on_body_exited(body: Node):
+	if body.name == "water_hit_box":
+		water_next_to -= 1
+	
 func move() -> void:
 	var direction = Vector2.ZERO
 	
@@ -21,7 +41,19 @@ func move() -> void:
 		direction = direction.normalized()
 	
 	velocity = direction * SPEED
-
-func _physics_process(delta: float) -> void:
-	move()
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	move()
+	
+	if water_next_to > 0 and Input.is_action_just_pressed("refill_water"):
+		water_level = 10
+	elif Input.is_action_just_pressed("refill_water") and water_level > 0:
+		var fire_objects = get_tree().get_nodes_in_group("fire")
+		var fire_died = false
+		for fire in fire_objects:
+			if(self.position.distance_to(fire.position) <= FIRE_DIS):
+				fire.queue_free()
+				fire_died = true
+		if fire_died:
+			water_level = 0			
